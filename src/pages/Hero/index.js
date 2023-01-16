ea2df6c6ed3b1669bf3b8287678589d91bfc1bed
Teska0075 <tushigregzen@gmail.com>
2023-01-17 01:02:13 +0800

    added Davids head will change that for sure

--- src/pages/Hero/index.js
+++ src/pages/Hero/index.js
@@ -3,6 +3,8 @@ import styles from "./style.module.scss";
 import { Typewriter } from "react-simple-typewriter";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Spline from "@splinetool/react-spline";
+
 const Hero = () => {
   return (
     <div className={styles.Container}>
@@ -28,7 +30,7 @@ const Hero = () => {
         </div>
       </div>
       <div className={styles.HeroItem}>
-        <motion.div
+        {/* <motion.div
         // initial={{ x: 0 }}
         // animate={{ x: 1500 }}
         // transition={{ duration: 10 }}
@@ -36,7 +38,9 @@ const Hero = () => {
           <div className={styles.ImgContainer}>
             <img src="https://cdn.discordapp.com/attachments/1027153692945825845/1063023430322114570/118149552_126221815851654_6049707491926780689_n.png"></img>
           </div>
-        </motion.div>
+        </motion.div> */}
+
+        <Spline scene="https://prod.spline.design/7JhsyO21PxBF59TH/scene.splinecode" />
       </div>
     </div>
   );
